27948a89de73c087394d62d4717f6bdbf3fe761b
Rachna <pichreamr@gmail.com>
2026-01-03 21:06:27 +0700

    Refactor page layout to implement scroll snapping for sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/sections/Hero";
+import Projects from "../components/sections/Projects";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -7,9 +8,17 @@ export default function Home() {
     <main className={styles.page}>
       <div className={styles.backdrop} aria-hidden="true" />
       <div className={styles.matrix} aria-hidden="true" />
-
       <Navbar />
-      <Hero />
+      <div className={styles.snapContainer} id="snap-root">
+        <section className={styles.snapSection} id="top">
+         <Hero />  
+        </section>
+       
+        <section className={styles.snapSection} id="projects">
+         <Projects />  
+        </section>
+      </div>
+      
     </main>
   );
 }
