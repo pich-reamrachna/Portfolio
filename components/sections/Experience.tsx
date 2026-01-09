13bfb6110b6ea6d2c17057eeba5ab90da00a9921
Rachna <pichreamr@gmail.com>
2026-01-10 03:34:08 +0700

    Added Experience Timeline Panels and placeholder for content

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -13,11 +13,22 @@ export default function Experience() {
 
             <div className={styles.experienceAllWrap}>
                 <div className={styles.experienceContainer}>
+                  {experience.map((exp) => (
+                      <div key={exp.role} className={styles.experiencePanel}>
+                        <div className={styles.timelineTitle}>
+                          <div className={styles.label}>{exp.year}</div>
+                          <div className={styles.role}>{exp.role}</div>
+                        </div>
+                        <span>{'>'}</span>
+                      </div>
+                      
+
+                  ))}
                 </div>
 
                 <div className={styles.timelineContainer}>
-                    <div className={styles.yearBox}>
-                        box
+                    <div className={styles.timelineContent}>
+                        
                     </div>
                 </div>
             </div>
