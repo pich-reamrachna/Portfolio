3e5f08f54514e864eba4225be5c9bdb39b80f595
Rachna <pichreamr@gmail.com>
2026-01-10 19:16:18 +0700

    Update metadataBase URL in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
+  metadataBase: new URL("https://portfolio-eight-ochre-uwuzusri1a.vercel.app"),
   title: "Rachna | Cybersecurity Student Portfolio",
   description: "A cybersecurity student passionate about hands-on learning through real-world projects and experiments. Curious to explore what I’m working on?",
   icons: {
